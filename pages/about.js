ae9503f081291f7486f8786fd285271fe7a1c263
abeTavarez <abeincwireless@gmail.com>
2021-10-17 13:45:55 -0400

    about page

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+
+      <p>Find all tech events related to IT, Development, Programming, DevOps, CyberSecurity and more...</p>
+
+      <p>Version: 1.0.0</p>
+    </div>
+  )
+}
