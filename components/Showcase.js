ae2f8c65ee528b602f63a7a26499d9d0f4dd78c9
abeTavarez <abeincwireless@gmail.com>
2021-10-18 20:11:15 -0400

    api

---
+++ components/Showcase.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from '@/styles/Showcase.module.css'
+
+export default function Showcase() {
+  return (
+    <div className={styles.showcase}>
+        <h1>Welcome to ITDevEvents</h1>
+        <h2>Find all tech events related to IT, Development and more...</h2>
+    </div>
+  )
+}
