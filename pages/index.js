95482f793be84687653378d77d779886e99ca0c5
abeTavarez <abeincwireless@gmail.com>
2021-10-18 21:23:18 -0400

    showing events on frontend

--- pages/index.js
+++ pages/index.js
@@ -3,20 +3,24 @@ import { API_URL } from '@/config/index'
 
 export default function HomePage({events}) {
   // this console.log will show on the browser console
-  console.log(events); // client side
+  // console.log(events); // client side
   return (
     <Layout>
       <h1>Upcoming Events</h1>
 
-      
+      {events.length === 0 && <h3>No Events To Show</h3>}
+
+      {events.map(evt => (
+        <h3 key={evt.id}>{evt.name}</h3>
+  ))}
     </Layout>
   )
 }
 
-//* ====== This function run server side
+//* ===================================== This function run server side
 // first we fetch the data then we return the data in the props
 // now we have access to the data (events) in the HomePage function props
-export async function getServerSideProps() {
+export async function getStaticProps() {
   // api_url comes from the config/index.js file
   const res = await fetch(`${API_URL}/api/events`) // fetch data locally
   const events = await res.json() // create events on json format
