656be9415f9065c84c942cc36fe105b20a21d6c7
abeTavarez <abeincwireless@gmail.com>
2021-10-19 21:06:39 -0400

    getting single event fromslug

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,13 +1,28 @@
 import {useRouter} from 'next/router'
 import Layout from '@/components/Layout';
+import { API_URL } from '@/config/index';
 
-export default function EventPage() {
+export default function EventPage({evt}) {
   const router = useRouter()
-  console.log(router);
+  // console.log(router);
   return (
     <Layout>
-      <h1>My event</h1>
+      <h1>{evt.name}</h1>
       
     </Layout>
   )
 }
+
+export async function getServerSideProps({query: {slug}}) { 
+  // console.log(slug);
+
+  // make request to the api using the slug from the query object
+  const res = await fetch(`${API_URL}/api/events/${slug}`)
+  // events is an array of events
+  const events = await res.json()
+  return {
+    props: {
+      evt: events[0]
+    }
+  }
+ }
