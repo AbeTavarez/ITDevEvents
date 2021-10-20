c3ef5fbaad46561f7692d46654dc21cf87336ed5
abeTavarez <abeincwireless@gmail.com>
2021-10-19 21:20:30 -0400

    getting events with getstaticprops and getstaticpaths

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -13,7 +13,23 @@ export default function EventPage({evt}) {
   )
 }
 
-export async function getServerSideProps({query: {slug}}) { 
+//* ============ Static way (for static websites) - which uses both getStaticPaths and getStaticProps
+export async function getStaticPaths() { 
+  const res = await fetch(`${API_URL}/api/events`)
+  const events = await res.json()
+
+  //* Creates all the paths with the slugs
+  // return array of objects  that was a params object with a slug object
+  const paths = events.map(evt => ({
+    params: {slug: evt.slug}
+  }))
+  return {
+    paths,
+    fallback: true
+  }
+ }
+
+export async function getStaticProps({params: {slug}}) { 
   // console.log(slug);
 
   // make request to the api using the slug from the query object
@@ -23,6 +39,26 @@ export async function getServerSideProps({query: {slug}}) {
   return {
     props: {
       evt: events[0]
-    }
+    },
+    revalidate: 1
   }
  }
+
+
+
+
+
+//* ===========================
+// export async function getServerSideProps({query: {slug}}) { 
+//   // console.log(slug);
+
+//   // make request to the api using the slug from the query object
+//   const res = await fetch(`${API_URL}/api/events/${slug}`)
+//   // events is an array of events
+//   const events = await res.json()
+//   return {
+//     props: {
+//       evt: events[0]
+//     }
+//   }
+//  }
